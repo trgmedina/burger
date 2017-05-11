5e42d45b5978b55874101ef4cdee89965ee77957
Theresa Medina <trgmedina@gmail.com>
2017-05-10 19:52:01 -0500

    Committing all code

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheezborger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger with Grilled Onions', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Grand Mac', false);
